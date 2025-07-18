b6171bf3c1524a9ed6163c997c888f9a8666a2ff
GenioZx <eli.rodri14@email.com>
2025-07-18 12:36:08 -0400

    commit ejs

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// Get /
+app.get('/', async (requestAnimationFrame, res) => {
+    res.send('hello')
+})
+
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
