2ca0d6d433d4f3b7733cfac858a672ab6c2e678b
GenioZx <eli.rodri14@email.com>
2025-07-18 13:21:37 -0400

    database not yet connected

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const dotenv = requre('dotenv');
+dotenv.config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI); 
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MONGODB ${mongoose.connection.name}`)}
+);
 
 // Get /git 
 app.get("/", async (req, res) => {
