f4580c360d36c4b17f007ffc6a2d7417f6463d5b
GenioZx <eli.rodri14@email.com>
2025-07-21 15:05:43 -0400

    fruit ID, create fruit/edit, created fruit/delete

--- server.js
+++ server.js
@@ -1,50 +1,74 @@
-const dotenv = require('dotenv');
-dotenv.config();
+const dotenv = require('dotenv'); // bringing the functionality of dotenv
+dotenv.config(); // using dotenv to bring the variables from the .env file
 
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI); 
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MONGODB ${mongoose.connection.name}`)}
-);
+// =============== model connection/logic ===============
+mongoose.connect(process.env.MONGODB_URI);
 
-//Import fruit model
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
+// Import the fruit model
 const Fruit = require('./models/fruit.js');
+// ======================================================
 
 // adding middleware for app
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
 
-
-// Get /git 
-app.get("/", async (req, res) => {
-  res.render("index.ejs");
+// GET /
+app.get('/', async (req, res) => {
+  res.render('index.ejs');
 });
 
 app.get('/fruits', async (req, res) => {
   const allFruits = await Fruit.find({});
 
-  res.render('fruits/index.ejs', { fruit: allFruits });
+  res.render('fruits/index.ejs', { fruits: allFruits });
 });
 
-// Get /fuits/new
+// GET /fruits/new
 app.get('/fruits/new', (req, res) => {
-  res.render('fruits/new.ejs')
-})
+  res.render('fruits/new.ejs');
+});
 
-// post /fruits
+app.get('/fruits/:fruitId', async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  
+  res.render('fruits/show.ejs', { fruit: foundFruit });
+});
+
+// POST /fruits
 app.post('/fruits', async (req, res) => {
   if (req.body.isReadyToEat === 'on') {
     req.body.isReadyToEat = true;
   } else {
     req.body.isReadyToEat = false;
   }
-  await Fruit.create(req.body); //This line is the database transaction from line 15
-  res.redirect('/fruits');
-})
 
+  await Fruit.create(req.body); // this line is the database transaction
+  res.redirect('/fruits');
+});
+
+// DELETE route
+app.delete('/fruits/:fruitId', async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect('/fruits');
+});
+
+app.get('/fruits/:fruitId/edit', async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+
+  res.render('fruits/edit.ejs', { fruit: foundFruit });
+})
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
 })
